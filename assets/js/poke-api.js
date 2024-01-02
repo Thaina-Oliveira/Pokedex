07206ce6124fe80f9c5968c6a36b97fa555a6f74
Thai Oliveira <138075601+Thaina-Oliveira@users.noreply.github.com>
2024-01-02 11:34:16 -0300

    Add files via upload

---
+++ assets/js/poke-api.js
@@ -0,0 +1,33 @@
+const pokeApi = {};
+
+function convertDetailApiToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
+  pokemon.photo = pokeDetail.sprites.other.home.front_default;
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  return pokemon;
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertDetailApiToPokemon);
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
